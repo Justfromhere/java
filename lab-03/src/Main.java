652d9e53421375e8921327b71fbd8fe0273c82b9
Vitaliy Present <vitaliy.p@thewhitelabelagency.com>
2024-10-02 10:57:44 +0300

    Б10

--- lab-03/src/Main.java
+++ lab-03/src/Main.java
@@ -99,17 +99,18 @@ public class Main {
         //Б9
         for (int i = 0; i < E1.length; i++) {
             for (int j = 0; j < E1[i].length; j++) {
-                System.out.print(E1[i][j]+ " ");
+//                System.out.print(E1[i][j]+ " ");
             }
-            System.out.println();
+//            System.out.println();
         }
 
         //Б10
-        for (int i=0; i < E.length;i++) {
-            for (int j=0; j< E[i].length; j++) {
-                int sum = + E[i][j];
-                System.out.println(sum);
+        for (int i = 0; i < E.length;i++) {
+            int sum =  0;
+            for (int j = 0; j < E[i].length; j++) {
+                sum += E[i][j];
             }
+            System.out.println(sum);
         }
     }
 }
